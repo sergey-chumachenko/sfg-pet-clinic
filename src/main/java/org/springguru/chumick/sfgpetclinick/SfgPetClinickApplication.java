81debef6ee3b764f09529eb72ef1500f6fd6ca13
serhii chumachenko <chumick@gmail.com>
2019-01-08 20:39:11 +0200

    app skeleton

---
+++ src/main/java/org/springguru/chumick/sfgpetclinick/SfgPetClinickApplication.java
@@ -0,0 +1,14 @@
+package org.springguru.chumick.sfgpetclinick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinickApplication.class, args);
+    }
+
+}
+
