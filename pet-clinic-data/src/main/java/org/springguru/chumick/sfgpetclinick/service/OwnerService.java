e3dab773c00c90a50a2630682f1e041f3688709c
Serhii Chumachenko <chumick@gmail.com>
2019-01-11 23:26:22 +0200

    common base service was added

--- pet-clinic-data/src/main/java/org/springguru/chumick/sfgpetclinick/service/OwnerService.java
+++ pet-clinic-data/src/main/java/org/springguru/chumick/sfgpetclinick/service/OwnerService.java
@@ -2,13 +2,7 @@ package org.springguru.chumick.sfgpetclinick.service;
 
 import org.springguru.chumick.sfgpetclinick.model.Owner;
 
-import java.util.Set;
-import java.util.function.Predicate;
+public interface OwnerService extends CrudService<Owner, Long> {
 
-public interface OwnerService {
 
-    Owner findBy(Predicate<Owner> ownerPredicate);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/org/springguru/chumick/sfgpetclinick/service/PetService.java
+++ pet-clinic-data/src/main/java/org/springguru/chumick/sfgpetclinick/service/PetService.java
@@ -2,12 +2,6 @@ package org.springguru.chumick.sfgpetclinick.service;
 
 import org.springguru.chumick.sfgpetclinick.model.Pet;
 
-import java.util.Set;
-import java.util.function.Predicate;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-    Pet findBy(Predicate<Pet> petPredicate);
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
