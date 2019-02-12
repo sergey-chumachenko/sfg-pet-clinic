221bd680187803ea309024637a2e1e1b47b41a15
Serhii_Chumachenko@epam.com <BinaryMiracles5>
2019-02-12 17:20:54 +0200

    fixed showing of the vets page

--- pet-clinic-web/src/main/java/org/springguru/chumick/sfgpetclinick/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/org/springguru/chumick/sfgpetclinick/controllers/VetsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetsController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listOfVets() {
         return "vets/index";
     }
